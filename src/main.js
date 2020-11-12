aaf9ea1a44a9b5d7639943d4894966b47bace05d
natsuki <362585052@qq.com>
2020-11-12 17:36:51 +0800

    'projectInit'

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@/assets/styles/reset.css'
+import '@/assets/styles/border.css'
+import fastClick from 'fastclick'
+fastClick.attach(document.body)
 
 createApp(App).use(router).mount('#app')
